dbdea68f6f885ce561e1cd3e9d176bafc186aebb
mohamedtamer0 <mt4452242@gmail.com>
2021-10-27 21:53:17 +0200

    Initial commit

---
+++ src/704. Binary Search.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        val index = -1
+        var begin = 0
+        var end = nums.size - 1
+        var middle = (end + begin) / 2
+        while (begin <= end) {
+            if (nums[middle] == target) return middle
+            if (nums[middle] > target) {
+                end = middle - 1
+                middle = (end + begin) / 2
+            }
+            if (nums[middle] < target) {
+                begin = middle + 1
+                middle = (end + begin) / 2
+            }
+        }
+        return index
+    }
+}

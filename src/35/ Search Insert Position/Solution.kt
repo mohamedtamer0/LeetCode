1972ed61af1c3aee97ed0a340a36f93d3690cf0d
mohamedtamer0 <mt4452242@gmail.com>
2021-11-06 05:20:39 +0200

    update

---
+++ src/35/ Search Insert Position/Solution.kt
@@ -0,0 +1,17 @@
+package `35`.` Search Insert Position`
+
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size - 1
+        while (low <= high) {
+            val mid = (low + high) ushr 1
+            when {
+                target < nums[mid] -> high = mid - 1
+                target > nums[mid] -> low = mid + 1
+                else -> return mid
+            }
+        }
+        return low
+    }
+}

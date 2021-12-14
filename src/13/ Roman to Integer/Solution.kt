5a9907143037eee1da344d28d29913d69c24563f
mohamedtamer0 <mt4452242@gmail.com>
2021-12-15 00:37:15 +0200

    update

---
+++ src/13/ Roman to Integer/Solution.kt
@@ -0,0 +1,27 @@
+package `13`.` Roman to Integer`
+
+class Solution {
+    private val Rm2I = mapOf(
+        'I' to 1, 'V' to 5,
+        'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000
+    )
+
+    fun romanToInt(s: String): Int {
+        var res = 0
+        var i = 0
+        while (i < s.length) {
+            if (i == s.length - 1) {
+                res += Rm2I[s[i]]!!
+                break
+            }
+            if (Rm2I[s[i]]!! < Rm2I[s[i + 1]]!!) {
+                res += Rm2I[s[i + 1]]!! - Rm2I[s[i]]!!
+                i += 2
+            } else {
+                res += Rm2I[s[i]]!!
+                i++
+            }
+        }
+        return res
+    }
+}
---
+++ src/9/ Palindrome Number/Solution.kt
@@ -0,0 +1,21 @@
+package `9`.` Palindrome Number`
+
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) {
+            return false
+        }
+
+        var a = IntArray(20)
+        var cnt = 0
+        var n = x
+        while (n > 0) {
+            a[cnt++] = n % 10
+            n /= 10
+        }
+        for (i in 0 until cnt / 2) {
+            if (a[i] != a[cnt - i - 1]) return false
+        }
+        return true
+    }
+}

1aeda9ac26a1eed54d98ce87ee7c883703fce2de
mohamedtamer0 <mt4452242@gmail.com>
2021-12-27 00:51:49 +0200

    update

--- src/797/ All Paths From Source to Target/Solution.java
+++ src/797/ All Paths From Source to Target/Solution.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
 
+        path.add(0);
+        deepSearch(graph, 0, path, res);
+
+        return res;
+    }
+
+    public void deepSearch(int[][] graph, int index, List<Integer> path, List<List<Integer>> res) {
+        if (index + 1 == graph.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int node : graph[index]) {
+            path.add(node);
+            deepSearch(graph, node, path, res);
+            path.remove(path.size() - 1);
+        }
     }
 }

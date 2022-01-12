ab3b7928d638e1e00148286eb2c3020fe129d38e
mohamedtamer0 <mt4452242@gmail.com>
2022-01-12 03:20:46 +0200

    update

---
+++ src/Solution.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
+        while (j >= 0)
+            nums1[k--] = if (i >= 0 && nums1[i] > nums2[j]) nums1[i--] else nums2[j--]
+        nums1.forEach { print("$it ") }; println()
+    }
+}
